90d2ebe2bc9762bf33796d1a569af8ef0b2e7d87
Eric Parker <eparkertamu96@gmail.com>
2023-07-11 09:05:17 -0500

    Fix errors found by pylint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+"""
+Main file for XYZ API Application
+"""
 from datetime import datetime
-from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
+    """Main API Method"""
     time = datetime.timestamp(datetime.now())
     return {"message": "Automate all the things!", "timestamp": time}
